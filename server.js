f736200bf28334c0d4418ffe5a7bd0e1841280c6
Rhythm Saha <rs.2001.saha@gmail.com>
2022-04-23 21:01:40 +0530

    server

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 require("./config/db");
 require("dotenv/config");
-
-// init app
 const app = express();
 
 // Import Routes
@@ -13,14 +11,9 @@ const authenticationRoute = require("./router/authenticationRoute");
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Api is running");
-});
-
 // Routes
 app.use("/auth", authenticationRoute);
 
-// Listen Server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log("server is running on port ", port);
