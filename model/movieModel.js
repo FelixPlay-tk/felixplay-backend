8ce6e5000e9e9edaad86b8c0685dc2e3758233b4
Rhythm Saha <rs.2001.saha@gmail.com>
2022-04-25 23:57:34 +0530

    movies model

---
+++ model/movieModel.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+
+const movieSchema = mongoose.Schema({
+    contentType: {
+        type: String,
+        required: true,
+        index: true,
+        lowercase: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        index: true,
+        lowercase: true,
+    },
+    details: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    language: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    poster: {
+        type: String,
+        required: true,
+    },
+    banner: {
+        type: String,
+        required: true,
+    },
+    categories: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "moviesCategory",
+            index: true,
+            lowercase: true,
+        },
+    ],
+
+    cast: [
+        {
+            type: String,
+            required: true,
+            index: true,
+            lowercase: true,
+        },
+    ],
+
+    runtime: {
+        type: String,
+        required: true,
+    },
+
+    releaseDate: {
+        type: Date,
+        required: true,
+        index: true,
+    },
+    region: {
+        type: String,
+        required: true,
+        index: true,
+        lowercase: true,
+    },
+
+    downloadLinks: [
+        {
+            resolution: { type: String },
+            link: { type: String, required: true },
+            size: { type: String, required: true },
+        },
+    ],
+
+    streamLink: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model("movie", movieSchema);
